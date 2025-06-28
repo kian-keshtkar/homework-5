46163b9de58068289ff0b95388e14e6b51a700d8
kianooo <85191405+kianooo@users.noreply.github.com>
2025-06-27 21:00:31 -0400

    Switched to InMemory DAO in MainWithInMemory.java

---
+++ src/main/java/app/MainWithInMemory.java
@@ -0,0 +1,64 @@
+package app;
+
+import java.awt.CardLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.WindowConstants;
+
+import data_access.InMemoryUserDataAccessObject;
+import entity.CommonUserFactory;
+import interface_adapter.ViewManagerModel;
+import interface_adapter.logged_in.LoggedInViewModel;
+import interface_adapter.login.LoginViewModel;
+import interface_adapter.signup.SignupViewModel;
+import view.LoggedInView;
+import view.LoginView;
+import view.SignupView;
+import view.ViewManager;
+
+/**
+ * The version of Main with an in-memory DAO used to store user data.
+ */
+public class MainWithInMemory {
+
+    /**
+     * The main method for starting the program with an in-memory DAO.
+     * @param args input to main
+     */
+    public static void main(String[] args) {
+        final JFrame application = new JFrame("Login Example");
+        application.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        final CardLayout cardLayout = new CardLayout();
+        final JPanel views = new JPanel(cardLayout);
+        application.add(views);
+
+        final ViewManagerModel viewManagerModel = new ViewManagerModel();
+        new ViewManager(views, cardLayout, viewManagerModel);
+
+        final LoginViewModel loginViewModel = new LoginViewModel();
+        final LoggedInViewModel loggedInViewModel = new LoggedInViewModel();
+        final SignupViewModel signupViewModel = new SignupViewModel();
+
+        final InMemoryUserDataAccessObject userDataAccessObject = new InMemoryUserDataAccessObject();
+
+        final SignupView signupView = SignupUseCaseFactory.create(viewManagerModel, loginViewModel,
+                signupViewModel, userDataAccessObject);
+        views.add(signupView, signupView.getViewName());
+
+        final LoginView loginView = LoginUseCaseFactory.create(viewManagerModel, loginViewModel,
+                loggedInViewModel, userDataAccessObject);
+        views.add(loginView, loginView.getViewName());
+
+        final LoggedInView loggedInView = ChangePasswordUseCaseFactory.create(viewManagerModel,
+                loggedInViewModel, userDataAccessObject);
+        views.add(loggedInView, loggedInView.getViewName());
+
+        viewManagerModel.setState(signupView.getViewName());
+        viewManagerModel.firePropertyChanged();
+
+        application.pack();
+        application.setVisible(true);
+    }
+}
